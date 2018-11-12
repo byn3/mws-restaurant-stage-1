2cdb65f9aa1573dce68ab44cef0deeeb85a87066
Bryan Leung <bryanleung92@gmail.com>
2018-11-11 21:05:37 -0800

    Add: offline cachine using a service worker

---
+++ js/sw_register.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker.register("./sw.js")
+	.then(function() {
+		console.log("Success!!!!");
+	})
+	.catch(function() {
+		console.log("POOP THE SW FAILED!!");
+	});
