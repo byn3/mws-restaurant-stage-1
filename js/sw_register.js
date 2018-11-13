d7b5ff11423fdc2a80792c216e31c566edef5082
Bryan Leung <bryanleung92@gmail.com>
2018-11-13 10:10:14 -0800

    Modified some things. not done yet

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,7 +1,7 @@
 /* code credit to Alexandro Perez and his walkthroughh */
 
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register("/sw.js").then(function (reg) {
+    navigator.serviceWorker.register("../sw.js").then(function (reg) {
         console.log("SW has been registered!");
     }).catch((e) => {
         console.log("Couldn't register SW \n", e);
--- sw.js
+++ sw.js
@@ -4,56 +4,60 @@ const cacheName = "restaurant-review-app";
 const staticCacheName = cacheName + "v1.0";
 const imgCache = cacheName + "img";
 
-let allCaches = [
-    staticCacheName,
-    imgCache
+let cacheV = [
+  staticCacheName,
+  imgCache
 ];
 
-self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll([
-                "/img/1.jpg",
-                "/img/2.jpg",
-                "/img/3.jpg",
-                "/img/4.jpg",
-                "/img/5.jpg",
-                "/img/6.jpg",
-                "/img/7.jpg",
-                "/img/8.jpg",
-                "/img/9.jpg",
-                "/img/10.jpg",
-                "/index.html",
-                "/restaurant.html",
-                "/css/styles.css",
-                "/js/dbhelper.js",
-                "/js/main.js",
-                "/js/restaurant_info.js",
-                "/data/restaurants.json"
-            ]);
-        })
-    );
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        "/img/1.jpg",
+        "/img/2.jpg",
+        "/img/3.jpg",
+        "/img/4.jpg",
+        "/img/5.jpg",
+        "/img/6.jpg",
+        "/img/7.jpg",
+        "/img/8.jpg",
+        "/img/9.jpg",
+        "/img/10.jpg",
+        "/index.html",
+        "/restaurant.html",
+        "/css/styles.css",
+        "/js/dbhelper.js",
+        "/js/main.js",
+        "/js/restaurant_info.js",
+        "/data/restaurants.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("activate", function (e) {
-    e.waitUntil(
-        caches.keys().then(function (names) {
-            return Promise.all(
-                names.filter(function (cache) {
-                    return cache.startsWith(cacheName) &&
-                        !cacheV.includes(cache);
-                }).map(function (cache) {
-                    return caches.delete(cache);
-                })
-            );
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(cache) {
+          return cache.startsWith(cacheName) &&
+            !cacheV.includes(cache);
+        }).map(function(cache) {
+          return caches.delete(cache);
         })
-    );
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') {
+        return;
+      } else {
+        return response || fetch(e.request);
+      }
+    })
+  );
 });
